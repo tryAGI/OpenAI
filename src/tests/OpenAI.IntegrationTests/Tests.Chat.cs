c8ca283e0a9ca2b7c9a101cee717c327c4697e35
Konstantin S <havendv@gmail.com>
2023-09-29 09:14:33 +0400

    test: Added CreateChatCompletionAsStreamAsync case.

--- src/tests/OpenAI.IntegrationTests/Tests.Chat.cs
+++ src/tests/OpenAI.IntegrationTests/Tests.Chat.cs
@@ -20,4 +20,24 @@ public partial class Tests
 
         Console.WriteLine(response.Choices.ElementAt(0).Message.Content);
     }
+    
+    [TestMethod]
+    public async Task CreateChatCompletionAsStreamAsync()
+    {
+        var api = GetAuthorizedApi();
+        var enumerable = api.CreateChatCompletionAsStreamAsync(new CreateChatCompletionRequest
+        {
+            Messages = new List<ChatCompletionRequestMessage>
+            {
+                "You are a helpful weather assistant.".AsSystemMessage(),
+                "What's the weather like today?".AsUserMessage(),
+            },
+            Model = ModelIds.Gpt35Turbo_0613,
+            User = "tryAGI.OpenAI.IntegrationTests.Tests.CreateChatCompletion",
+        });
+        await foreach (var response in enumerable)
+        {
+            Console.WriteLine(response.Choices.ElementAt(0).Delta.Content);
+        }
+    }
 }
