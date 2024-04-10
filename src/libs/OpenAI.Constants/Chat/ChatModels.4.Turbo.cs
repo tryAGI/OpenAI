0773b3963bc0dbde550dab0212945f91a4a26293
HavenDV <havendv@gmail.com>
2024-04-10 19:20:04 +0400

    feat: Added Gpt4Turbo.

--- src/libs/OpenAI.Constants/Chat/ChatModels.4.Turbo.cs
+++ src/libs/OpenAI.Constants/Chat/ChatModels.4.Turbo.cs
@@ -25,10 +25,23 @@ public readonly partial record struct ChatModels
     /// Training data: Up to Apr 2023 <br/>
     /// </summary>
     internal const string Gpt4_0125_PreviewId = "gpt-4-0125-preview";
+    
+    /// <summary>
+    /// GPT-4 Turbo with Vision model.
+    /// Vision requests can now use JSON mode and function calling. <br/>
+    /// Max input tokens: 128,000 tokens <br/>
+    /// Max output tokens: 4,096 tokens <br/>
+    /// Training data: Up to Dec 2023 <br/>
+    /// </summary>
+    internal const string Gpt4Turbo_2024_04_09Id = "gpt-4-turbo-2024-04-09";
 
     /// <inheritdoc cref="Gpt4_0125_PreviewId"/>
     /// <remarks>Alias - Currently points to gpt-4-0125-preview.</remarks>
     internal const string Gpt4TurboPreviewId = "gpt-4-turbo-preview";
+
+    /// <inheritdoc cref="Gpt4Turbo_2024_04_09Id"/>
+    /// <remarks>Alias - Currently points to gpt-4-turbo-2024-04-09.</remarks>
+    internal const string Gpt4TurboId = "gpt-4-turbo";
     
     /// <inheritdoc cref="Gpt4_1106_PreviewId"/>
     public static ChatModels Gpt4_1106_Preview { get; } = new(
@@ -44,9 +57,22 @@ public readonly partial record struct ChatModels
         PricePerInputTokenInUsd: 10.0 * UsdPerMillionTokens,
         PricePerOutputTokenInUsd: 30.0 * UsdPerMillionTokens);
     
+    /// <inheritdoc cref="Gpt4Turbo_2024_04_09Id"/>
+    public static ChatModels Gpt4Turbo_2024_04_09 { get; } = new(
+        Id: Gpt4Turbo_2024_04_09Id,
+        ContextLength: 128_000,
+        PricePerInputTokenInUsd: 10.0 * UsdPerMillionTokens,
+        PricePerOutputTokenInUsd: 30.0 * UsdPerMillionTokens);
+    
     /// <inheritdoc cref="Gpt4TurboPreviewId"/>
     public static ChatModels Gpt4TurboPreview { get; } = Gpt4_0125_Preview with
     {
         Id = Gpt4TurboPreviewId,
+    };
+    
+    /// <inheritdoc cref="Gpt4TurboId"/>
+    public static ChatModels Gpt4Turbo { get; } = Gpt4Turbo_2024_04_09 with
+    {
+        Id = Gpt4TurboId,
     };
 }
