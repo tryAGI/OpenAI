27c35ca8987b547cec3f835e5f3e222fa52792fc
HavenDV <havendv@gmail.com>
2024-07-10 09:01:32 +0400

    test: Fixed.

--- src/tests/OpenAI.Generators.SnapshotTests/SnapshotTests.cs
+++ src/tests/OpenAI.Generators.SnapshotTests/SnapshotTests.cs
@@ -10,20 +10,8 @@ public class IpcGeneratorSnapshotTests : VerifyBase
     }
     
     [TestMethod]
-    public Task GeneratesUpdateCodeCorrectly()
-    {
-        return this.CheckSourceAsync(Resources.UpdateCodeFunctions_cs.AsString());
-    }
-    
-    [TestMethod]
     public Task VariousTypes()
     {
         return this.CheckSourceAsync(Resources.VariousTypesFunctions_cs.AsString());
-    }
-    
-    [TestMethod]
-    public Task MaintenanceFunctionsTypes()
-    {
-        return this.CheckSourceAsync(Resources.MaintenanceFunctions_cs.AsString());
     }
 }
