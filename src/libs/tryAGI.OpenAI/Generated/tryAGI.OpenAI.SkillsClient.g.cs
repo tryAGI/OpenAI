e470e5e7f15cbf7167b8b93b5cd7a9dd74ac1e77
Konstantin S. <havendv@gmail.com>
2026-08-14 19:41:21 +0400

    feat: Updated OpenAPI spec (#381)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/tryAGI.OpenAI/Generated/tryAGI.OpenAI.SkillsClient.g.cs
+++ src/libs/tryAGI.OpenAI/Generated/tryAGI.OpenAI.SkillsClient.g.cs
@@ -33,10 +33,17 @@ namespace tryAGI.OpenAI
 
         /// <inheritdoc/>
         public global::tryAGI.OpenAI.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::tryAGI.OpenAI.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::tryAGI.OpenAI.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
